5f5631a6af4f05dc5836ce2da346164eebb241d0
Mangpha <8350130@gmail.com>
2023-01-26 16:33:10 +0900

    Favorites

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webtoon/constants.dart';
 import 'package:flutter_webtoon/models/webtoon_detail_model.dart';
 import 'package:flutter_webtoon/models/webtoon_episode_model.dart';
 import 'package:flutter_webtoon/services/api_service.dart';
 import 'package:flutter_webtoon/widgets/episode_widget.dart';
 import 'package:flutter_webtoon/widgets/webtoon_image.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -22,12 +24,45 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList(LIKED_TOONS);
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        isLiked = true;
+      }
+    } else {
+      await prefs.setStringList(LIKED_TOONS, []);
+    }
+    setState(() {});
+  }
 
   @override
   void initState() {
     super.initState();
+    // Webtoon Api Init
     webtoon = ApiService.getToonById(widget.id);
     episodes = ApiService.getLatestEpisodesById(widget.id);
+
+    // Preferences Init
+    initPrefs();
+  }
+
+  onFavTap() async {
+    final likedToons = prefs.getStringList(LIKED_TOONS);
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList(LIKED_TOONS, likedToons);
+      isLiked = !isLiked;
+    }
+    setState(() {});
   }
 
   @override
@@ -44,6 +79,13 @@ class _DetailScreenState extends State<DetailScreen> {
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         elevation: 2,
+        actions: [
+          IconButton(
+            onPressed: onFavTap,
+            icon:
+                Icon(isLiked ? Icons.favorite : Icons.favorite_border_outlined),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
